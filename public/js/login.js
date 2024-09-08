1829d3f873e679c985d47af18cde2e06e6b01329
Nguyễn Việt Dũng <147736151+Wjndz@users.noreply.github.com>
2024-09-08 18:53:59 +0700

    them anh

--- public/js/login.js
+++ public/js/login.js
@@ -13,7 +13,12 @@ $(document).on('submit', '#loginForm', function(e) {
     .then(response => {
         if (!response.ok) {
             return response.json().then(data => {
-                throw new Error(data.error || 'Đăng nhập thất bại'); 
+                // Kiểm tra thông báo lỗi từ server
+                if (data.error === 'Invalid credentials') {
+                    throw new Error('Tên đăng nhập hoặc mật khẩu không đúng');
+                } else {
+                    throw new Error(data.error || 'Đăng nhập thất bại'); 
+                }
             });
         }
         return response.json();
@@ -22,8 +27,8 @@ $(document).on('submit', '#loginForm', function(e) {
         localStorage.setItem('token', data.token);
 
         // Cập nhật giao diện sau khi đăng nhập thành công
-        $('#user-info').text(`Welcome, ${username}`); // Hiển thị tên người dùng
-        $('#auth-btn').text('Logout'); // Đổi nút "Login" thành "Logout"
+        $('#user-info').text(`Welcome, ${username}`); 
+        $('#auth-btn').text('Logout'); 
         $('#auth-btn').off('click').on('click', function () {
             localStorage.removeItem('token'); 
             location.reload(); 
